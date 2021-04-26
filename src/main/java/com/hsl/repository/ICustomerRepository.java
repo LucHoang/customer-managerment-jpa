2eabe40505d111dd351a1641bc0f22d9dfd17ee9
LucHoang <hoangsyluc@gmail.com>
2021-04-26 22:05:32 +0700

    Ứng dụng quản lý khách hàng

---
+++ src/main/java/com/hsl/repository/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.hsl.repository;
+
+import com.hsl.model.Customer;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+}
---
+++ src/main/java/com/hsl/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.hsl.service;
+
+import com.hsl.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
